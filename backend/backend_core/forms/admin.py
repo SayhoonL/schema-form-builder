6adc599339d4b8ac36cfc4878bdfbf964bb33ee5
SayhoonL <97651310+SayhoonL@users.noreply.github.com>
2025-12-25 02:25:33 -0500

    Delete backend/backend_core directory

--- backend/backend_core/forms/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Form, FormSubmission
-
-admin.site.register(Form)
-admin.site.register(FormSubmission)
--- backend/backend_core/forms/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import FormListCreateView, FormDetailView, FormSubmitView
-
-urlpatterns = [
-    path('forms/', FormListCreateView.as_view(), name='form-list-create'),
-    path('forms/<int:pk>/', FormDetailView.as_view(), name='form-detail'),
-    path('forms/<int:pk>/submit/', FormSubmitView.as_view(), name='form-submit'),
-]
